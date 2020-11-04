abe1b8d57215bb7f25ea912f3da403ec5b4849f0
Herbert Máté <herbertsticky@gmail.com>
2020-11-04 13:01:36 +0100

    Adding Admin role seed

--- backend/OnlineComputerShop.IdentityProvider/Program.cs
+++ backend/OnlineComputerShop.IdentityProvider/Program.cs
@@ -3,17 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OnlineComputerShop.Dal;
 
 namespace OnlineComputerShop.IdentityProvider
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var services = host.Services.CreateScope())
+            {
+                var dbContext = services.ServiceProvider.GetRequiredService<OnlineComputerShopContext>();
+                await dbContext.Database.MigrateAsync();
+
+                var roleMgr = services.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                var adminRole = new IdentityRole("Admin");
+
+                if (!dbContext.Roles.Any())
+                {
+                    await roleMgr.CreateAsync(adminRole);
+                }
+
+                
+               
+            }            
+            
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
