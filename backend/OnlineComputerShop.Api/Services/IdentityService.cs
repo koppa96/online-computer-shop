3278f3c183336eed9495d6d45256e1a11a6815b9
Herbert Máté <herbertsticky@gmail.com>
2020-11-04 13:13:20 +0100

    Adding IIdentityService and IdentityService

---
+++ backend/OnlineComputerShop.Api/Services/IdentityService.cs
@@ -0,0 +1,29 @@
+﻿using OnlineComputerShop.Application.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+
+namespace OnlineComputerShop.Api.Services
+{
+    public class IdentityService : IIdentityService
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public IdentityService(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public Guid GetUserId()
+        {
+            return Guid.Parse(
+                httpContextAccessor.HttpContext.User.Claims
+                .SingleOrDefault(x => x.Type.ToLower() == "sub")
+                .Value
+            );
+        }
+    }
+}
---
+++ backend/OnlineComputerShop.Application/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace OnlineComputerShop.Application.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+        public Guid GetUserId();
+    }
+}
