23040b0a47811c4997c5615939233950d6a8a25a
Herbert Máté <herbertsticky@gmail.com>
2020-11-25 00:43:37 +0100

    Removing OnInit from RemoveConfirmDialogComponent

--- frontend/admin/src/app/shared/remove-confirm-dialog/remove-confirm-dialog.component.ts
+++ frontend/admin/src/app/shared/remove-confirm-dialog/remove-confirm-dialog.component.ts
@@ -6,12 +6,9 @@ import { NbDialogRef } from '@nebular/theme';
   templateUrl: './remove-confirm-dialog.component.html',
   styleUrls: ['./remove-confirm-dialog.component.scss']
 })
-export class RemoveConfirmDialogComponent implements OnInit {
+export class RemoveConfirmDialogComponent {
 
   constructor(private dialogRef: NbDialogRef<RemoveConfirmDialogComponent>) { }
-
-  ngOnInit(): void {
-  }
 
   onConfirm() {
     this.dialogRef.close(true);
